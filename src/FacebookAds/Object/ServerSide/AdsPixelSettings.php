312956abfbd2627031e26fed701d944e97872b8b
webdev0x1 <82759103+webdev0x1@users.noreply.github.com>
2022-09-05 21:55:49 +0530

    PHP 8.1 compatibility

--- src/FacebookAds/Object/ServerSide/AdsPixelSettings.php
+++ src/FacebookAds/Object/ServerSide/AdsPixelSettings.php
@@ -151,8 +151,11 @@ class AdsPixelSettings implements ArrayAccess {
    * @param integer $offset Offset
    * @return boolean
    */
-  public function offsetExists($offset) {
-    return isset($this->container[$offset]);
+  public function offsetExists($offset) :bool {
+    if(isset($this->container[$offset]))
+      return true;
+      
+    return false;
   }
 
   /**
@@ -160,8 +163,10 @@ class AdsPixelSettings implements ArrayAccess {
    * @param integer $offset Offset
    * @return mixed
    */
-  public function offsetGet($offset) {
-    return isset($this->container[$offset]) ? $this->container[$offset] : null;
+  public function offsetGet($offset) :bool {
+    if(isset($this->container[$offset]))
+      return $this->container[$offset];
+    return  null;
   }
 
   /**
@@ -170,7 +175,7 @@ class AdsPixelSettings implements ArrayAccess {
    * @param mixed $value Value to be set
    * @return void
    */
-  public function offsetSet($offset, $value) {
+  public function offsetSet($offset, $value) :void {
     if (is_null($offset)) {
       $this->container[] = $value;
     } else {
@@ -183,7 +188,7 @@ class AdsPixelSettings implements ArrayAccess {
    * @param integer $offset Offset
    * @return void
    */
-  public function offsetUnset($offset) {
+  public function offsetUnset($offset) :void {
     unset($this->container[$offset]);
   }
 
